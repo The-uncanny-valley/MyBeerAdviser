79521309c83589035a98cd9dd231c5345e311660
the-uncanny-valley <runcannyvalley@gmail.com>
2025-07-17 21:37:32 +0300

    Implement beer recommendation logic in MainActivity

--- app/src/main/java/com/example/mybeeradviser/MainActivity.kt
+++ app/src/main/java/com/example/mybeeradviser/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.mybeeradviser
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,27 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val findBeer = findViewById<Button>(R.id.find_beer)
+        findBeer.setOnClickListener {
+            val beerColor = findViewById<Spinner>(R.id.beer_color)
+            val color = beerColor.selectedItem
+
+            val beerList = getBeers(color.toString())
+
+            val beers = beerList.joinToString("\n")
+            val brands = findViewById<TextView>(R.id.brands)
+
+            brands.text = beers
+        }
+    }
+}
+
+fun getBeers(color: String): List<String> {
+    return when(color) {
+        "light" -> listOf("Jail Pale Ale", "Lager Lite")
+        "amber" -> listOf("Jack Amber", "Red Moose")
+        "brown" -> listOf("Brown Bear Beer", "Bock Brownie")
+        else -> listOf("Gout Stout", "Dark Daniel")
     }
 }
