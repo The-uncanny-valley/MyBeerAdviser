fd5add5687bea6cbc4c250463c6e48b85a3f3d97
the-uncanny-valley <runcannyvalley@gmail.com>
2025-07-20 15:24:45 +0300

    Add SplashActivity with layout and manifest configuration

---
+++ app/src/main/java/com/example/mybeeradviser/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.mybeeradviser
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private val splashDelay: Long = 2000 // 2 секунды
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashDelay)
+    }
+}
